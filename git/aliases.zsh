fba1f2aca7f877e301e44cd48e89c8592e8caf05
R. Kevin Nelson <kevin@rkn.la>
2013-07-10 14:55:47 -0700

    Update git things with my git things.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,20 +1,21 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
+#hub_path=$(which hub)
+#if (( $+commands[hub] ))
+#then
+#  alias git=$hub_path
+#fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+# Git alias fun time!
+gd() { git diff $* | view -; }
+gdc() { gd --cached $*; }
+alias gx="gitx"
+alias gxa="gitx --all"
+alias gb="git branch"
+alias gbl="gb"
+alias gs="git st -sb"
+alias d="git diff"
+alias gco="git checkout $*"
+
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
