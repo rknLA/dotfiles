66a881362d9489e26bb0aa28c7759ba072f4fa22
R. Kevin Nelson <kevin@rkn.la>
2013-07-10 19:44:03 -0700

    Add Rdio aliases and path.

---
+++ rdio/aliases.zsh
@@ -0,0 +1,3 @@
+alias rdio-server='cd ~/code/rdio/web && ./apply_configuration.py dev && ./manage.py runcpdebug 0.0.0.0:8000' 
+alias rdio='cd ~/code/rdio/web/rdio'
+alias ios='cd ~/code/rdio/ios-sdk/api'
---
+++ rdio/path.zsh
@@ -0,0 +1,1 @@
+export PATH=$PATH:~/code/rdio/env/arc/arcanist/bin
