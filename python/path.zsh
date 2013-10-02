421fdab1998620ec051a761c54de7dc1620c3ee5
R. Kevin Nelson <kevin@rkn.la>
2013-10-01 19:32:10 -0700

    Add virtualenvwrapper stuff.

---
+++ python/path.zsh
@@ -0,0 +1,4 @@
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/code
+source /usr/local/bin/virtualenvwrapper.sh
+export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
