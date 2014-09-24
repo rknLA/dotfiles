71228b5270e29cb81025ffcab5a9d6b9acda10bd
R. Kevin Nelson <kevin@rkn.la>
2014-09-24 17:41:50 +0200

    Fix ruby.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH:/usr/local/Cellar/ruby193/1.9.3-p448/bin"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
