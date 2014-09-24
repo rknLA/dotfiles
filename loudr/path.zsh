75698d62374201bd377ca439b2cc7ebf53a82871
R. Kevin Nelson <kevin@rkn.la>
2014-09-24 17:43:03 +0200

    Fix Loudr

--- loudr/path.zsh
+++ loudr/path.zsh
@@ -1,3 +1,3 @@
-source '/Users/kevin/code/Loudr/google-cloud-sdk/path.zsh.inc'
+source '/Users/kevin/google-cloud-sdk/path.zsh.inc'
 export PATH=$PATH:~/google-cloud-sdk/bin
 export PYTHONPATH=$PYTHONPATH:~/google-cloud-sdk/platform/google_appengine
