5d2165a1016066d49b5cd7edf023c2c71222bde0
Kevin Nelson <r.kevin.nelson@artory.com>
2016-10-27 02:05:41 -0700

    remove loudr config for artory machine

--- loudr/aliases.zsh
+++
@@ -1,6 +0,0 @@
-alias loudr='cd ~/Code/Loudr/'
-alias loudrweb='cd ~/Code/Loudr/loudr-web && workon loudr'
-alias loudrapi='cd ~/Code/Loudr/api-gateway && source venv/bin/activate'
-alias loudrserver='loudr && ./google-cloud-sdk/platform/google_appengine/dev_appserver.py --require_indexes YES --log_level warning --host=0.0.0.0 --port 9001 --admin_port 8002 -A rdiscover ./rdstore/'
-
-alias pale='cd ~/Code/Loudr/pale && workon pale'
--- loudr/path.zsh
+++
@@ -1,3 +0,0 @@
-source '/Users/kevin/google-cloud-sdk/path.zsh.inc'
-export PATH=$PATH:~/google-cloud-sdk/bin
-export PYTHONPATH=$PYTHONPATH:~/google-cloud-sdk/platform/google_appengine
