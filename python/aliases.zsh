26787a98b5cc67145264bdcd8444e00c581337d7
R. Kevin Nelson <kevin@rkn.la>
2016-10-27 10:46:16 +0200

    who knows, really.

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,2 +1,6 @@
-alias rmpyc='find . -name \*.pyc -delete'
+alias rmpyc='find . -name \*.pyc -delete; find . -name __pycache__ -delete;'
 alias workoff='deactivate'
+
+activate() {
+    source $1/bin/activate
+}
--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,17 +1,18 @@
+eval "$(rbenv init -)"
 # rehash shims
-rbenv rehash 2>/dev/null
+#rbenv rehash 2>/dev/null
 
 # shell thing
-rbenv() {
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
-
-  case "$command" in
-  shell)
-    eval `rbenv "sh-$command" "$@"`;;
-  *)
-    command rbenv "$command" "$@";;
-  esac
-}
+#rbenv() {
+#  command="$1"
+#  if [ "$#" -gt 0 ]; then
+#    shift
+#  fi
+#
+#  case "$command" in
+#  shell)
+#    eval `rbenv "sh-$command" "$@"`;;
+#  *)
+#    command rbenv "$command" "$@";;
+#  esac
+#}
