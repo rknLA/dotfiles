11f787a384371f2387208e1df0e2c4990d358b5b
R. Kevin Nelson <kevin@rkn.la>
2017-01-26 09:56:09 +0100

    Minor tweaks from regular work

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -6,8 +6,10 @@ then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
+  alias lla="gls -la --color"
   alias la='gls -A --color'
 else
+  alias l="ls -lAh"
   alias ls="ls -G"
   alias ll="ls -lG"
   alias lla="ls -laG"
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,12 +39,16 @@ setopt complete_aliases
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+bindkey -e
+
+bindkey '\e[A' history-beginning-search-backward
+bindkey '\e[B' history-beginning-search-forward
+#bindkey '^[^[[D' backward-word
+#bindkey '^[^[[C' forward-word
+#bindkey '^[[5D' beginning-of-line
+#bindkey '^[[5C' end-of-line
+#bindkey '^[[3~' delete-char
+#bindkey '^[^N' newtab
+#bindkey '^?' backward-delete-char
+#bindkey '^[[A' up-line-or-search
+#bindkey '^[[B' down-line-or-search
