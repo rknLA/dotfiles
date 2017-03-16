e2aea00de30902fcb6ca394af3db99187cfc4e68
Kevin Nelson <r.kevin.nelson@artory.com>
2017-03-16 14:18:46 +0100

    docker cleanup, command t, pdb shortcut

--- docker/env.zsh
+++
@@ -1,6 +0,0 @@
-dstatus="$(docker-machine status default)"
-if [[ "$dstatus" == "Stopped" ]]; then
-    echo "Docker is stopped. run \`docker-machine start default\` to turn it on"
-else
-    eval $(docker-machine env default)
-fi
